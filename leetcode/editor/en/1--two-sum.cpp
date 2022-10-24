e94e161576c57522c6a4adfc3079b6f2cb0a4928
VincentXu <vincentxu1998@outlook.com>
2022-10-24 15:25:48 -0400

    Initial Commit

---
+++ leetcode/editor/en/1--two-sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout<<"Hello LeetCode"<<endl;
+}
